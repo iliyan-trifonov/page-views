d3839c095cb8d6066bc2354240d8a720d80e62b3
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-10-25 04:22:35 -0400

    Applied fixes from StyleCI

--- modules/AdapterInterface.php
+++ modules/AdapterInterface.php
@@ -5,8 +5,12 @@ namespace ITrifonov\PageViews\modules;
 interface AdapterInterface
 {
     public function setServer($server);
+
     public function setConfig($config);
+
     public function init();
+
     public function get();
+
     public function incr($domain);
 }
